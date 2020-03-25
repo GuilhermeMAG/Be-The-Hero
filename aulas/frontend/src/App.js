f486255c98a4dd35914c3d5deb6f5a4ad26e0cc8
GuilhermeMAG <g.casagrande@uni9.edu.br>
2020-03-25 17:01:26 -0300

    Revert "Create Logon Page"
    This reverts commit d9829f5fdb02590b873b99a6d835bb5611f0712c.

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
-
-import './global.css';
-
-import Logon from './pages/logon/index';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Logon /> 
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
